11809b8676bcbd53681f5468108299c715afbf0e
FlorCredifiar <florencia.roth@credifiar.com.ar>
2024-05-13 18:44:19 -0300

    update component - products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Search } from './Search';
+import { ProductContext } from "../contexts/ProductContext"
+import { Spinner } from './Spinner';
+import { Pagination } from './Pagination';
+
+
 
 
 export const Products = () => {
 
     const [showActionDropdown, setShowActionDropdown] = useState(false);
     const [selectedOrder, setSelectedOrder] = useState("Ordernar por");
+    const [selectedCategory, setSelectedCategory] = useState();
+    const { state, getProducts, getProductsByCategory } = useContext(ProductContext);
+    const [products, setProducts] = useState([]);
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [search, setSearch] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    useEffect(() => {
+        setProducts(state.products);
+        setIsLoading(false)
+    }, [state.products]);
+
+    useEffect(() => {
+        if(selectedCategory && selectedCategory !=='Todas'){
+            setIsLoading(true);
+            setCurrentPage(1);
+            getProductsByCategory(selectedCategory);
+        }
+        else init();
+    }, [selectedCategory]);
+
+    const init = () => {
+        setIsLoading(true);
+        getProducts(); 
+    };
 
     const toggleActionDropdown = () => {
         setShowActionDropdown(!showActionDropdown);
@@ -14,12 +50,33 @@ export const Products = () => {
     const handleOrderSelect = (order) => {
         setSelectedOrder(order);
         toggleActionDropdown(); 
+        setSortOrder(order === "Menor Precio" ? "asc" : "desc");
+    };
+
+    const sortedProducts = () => {
+        let filteredProducts = [...products];
+        if (search.trim() !== "") {
+            filteredProducts = filteredProducts.filter(product =>
+                product.title.toLowerCase().includes(search.toLowerCase())
+            );
+        }
+
+        if (sortOrder === "asc") {
+            return filteredProducts.sort((a, b) => a.price - b.price);
+        }
+        return filteredProducts.sort((a, b) => b.price - a.price);
+    };
+
+    const getCurrentPageProducts = () => {
+        const startIndex = (currentPage - 1) * 10;
+        const endIndex = startIndex + 10;
+        return sortedProducts().slice(startIndex, endIndex);
     };
 
     return (
         <>
-            <div className='ml-20 sm:ml-64 mt-5'>
-                <Search/>
+            <div className='my-24 mx-5'>
+                <Search selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} setSearch={setSearch}/>
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-4 mx-5">
                     <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
                         <div>
@@ -33,17 +90,21 @@ export const Products = () => {
                                 <div id="dropdownAction" className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                     <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
                                         <li>
-                                            <a href="#" onClick={() => handleOrderSelect("Mayor Precio")} className={`block px-4 py-2 ${selectedOrder === "Mayor Precio" ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'}`}>Mayor Precio</a>
+                                            <a onClick={() => handleOrderSelect("Mayor Precio")} className={`block px-4 py-2 ${selectedOrder === "Mayor Precio" ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'}`}>Mayor Precio</a>
                                         </li>
                                         <li>
-                                            <a href="#" onClick={() => handleOrderSelect("Menos Precio")} className={`block px-4 py-2 ${selectedOrder === "Menos Precio" ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'}`}>Menos precio</a>
+                                            <a onClick={() => handleOrderSelect("Menor Precio")} className={`block px-4 py-2 ${selectedOrder === "Menor Precio" ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'}`}>Menor precio</a>
                                         </li>
                                     </ul>
                                 </div>
                             )}
                         </div>
                     </div>
-                    
+                    {isLoading ? (
+                        <div className="w-full justify-center flex">
+                            <Spinner/>
+                        </div>
+                            ) : ( 
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-6">
                         <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
                             <tr>
@@ -57,83 +118,43 @@ export const Products = () => {
                                     Categoria
                                 </th>
                                 <th scope="col" className="px-6 py-3">
-                                    Precio
+                                    Precio ($)
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="border-b border-gray-200 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                    Apple MacBook Pro 17"
-                                </th>
-                                <td className="px-6 py-4">
-                                    Silver
-                                </td>
-                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                    Laptop
-                                </td>
-                                <td className="px-6 py-4">
-                                    $2999
-                                </td>
-                            </tr>
-                            <tr className="border-b border-gray-200 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                    Microsoft Surface Pro
-                                </th>
-                                <td className="px-6 py-4">
-                                    White
-                                </td>
-                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                    Laptop PC
-                                </td>
-                                <td className="px-6 py-4">
-                                    $1999
-                                </td>
-                            </tr>
-                            <tr className="border-b border-gray-200 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                    Magic Mouse 2
-                                </th>
-                                <td className="px-6 py-4">
-                                    Black
-                                </td>
-                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                    Accessories
-                                </td>
-                                <td className="px-6 py-4">
-                                    $99
-                                </td>
-                            </tr>
-                            <tr className="border-b border-gray-200 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                    Google Pixel Phone
-                                </th>
-                                <td className="px-6 py-4">
-                                    Gray
-                                </td>
-                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                    Phone
-                                </td>
-                                <td className="px-6 py-4">
-                                    $799
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                    Apple Watch 5
-                                </th>
-                                <td className="px-6 py-4">
-                                    Red
-                                </td>
-                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                    Wearables
-                                </td>
-                                <td className="px-6 py-4">
-                                    $999
-                                </td>
-                            </tr>
+                            {getCurrentPageProducts().length !== 0 ? (
+                                getCurrentPageProducts().map((product, index) =>  (
+                                    <tr key={index} className="border-b border-gray-200 dark:border-gray-700">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                            <img src={product.image} className="w-16 max-w-full max-h-full" alt={`product-${product.id}`}/>
+                                        </th>
+                                        <td className="px-6 py-4 w-1/3" >
+                                        {product.title}
+                                        </td>
+                                        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                        {product.category}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                        {product.price.toFixed(2)}
+                                        </td>
+                                    </tr>
+                                ))
+                            )
+                            : 
+                            (
+                                <tr>
+                                    <td colSpan="4" className="px-6 py-4">
+                                        <p className='w-full text-center'>Sin resultados.</p>
+                                    </td>
+                                </tr>
+                            )
+                            }
                         </tbody>
+
                     </table>
+                )}
+                <Pagination totalProducts={sortedProducts().length} currentPage={currentPage} setCurrentPage={setCurrentPage} getCurrentPageProducts={getCurrentPageProducts}/>
                 </div>
             </div>
 
